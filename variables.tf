6120dde801df67241cbbe92f7b065c665e2de3e9
Leandro Magalhaes <leandro@foodbomb.com.au>
2022-10-28 09:52:03 +1100

    Add description to module input variables

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "cluster_name" {
   type        = string
-  description = "Cluster name where the migration container will run."
+  description = "Cluster name where the migration will run."
 }
 
 variable "migration_container_name" {
   type        = string
-  description = "Name of the container who is going to run the migration"
+  description = "Name to be given to the new migration container."
 }
 
 variable "override_template" {
   type        = map(any)
-  description = "The override_template map."
+  description = "The override_template map. For information about how to override a container definition check <a href='https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_ContainerOverride.html' target='_blank'>AWS ECS ContainerOverride documentation</a>."
 }
 
 variable "task_definition_arn" {
   type        = string
-  description = "Original task definition arn, whose values from the override_template will take place."
+  description = "ARN of the original task definition to use as base for the migration."
 }
 
 variable "region" {
   type        = string
-  description = "AWS region where the cluster is located"
+  description = "Cluster AWS region"
 }
